1a5dabe042fa281cd046f3b3f7ca6196a28a8116
ABHIRAM KRISHNA S <abhiramkrishna085@gmail.com>
2023-05-02 14:51:28 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     Hello
-    </div>
+    <>
+      <div className="my-5">
+        <h1 className="text-5xl text-center font-bold">CleanBot</h1>
+      </div>
+    </>
   );
 }
 
